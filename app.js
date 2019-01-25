d2398d85234eea8ca7530cd15d09bb85758b9a2c
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-01-25 09:29:00 -0500

    initial stack of cards

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // setup
 var app = new PIXI.Application(window.screen.width, window.screen.height, {backgroundColor : 0x0});
 document.body.appendChild(app.view);
+
+// create a 144 sprites from image path
+var cards = [];
+for (let i = 0; i < 144; i++) {
+    cards.push(PIXI.Sprite.fromImage('imgs/trapcard.png'));
+    cards[i].x = (app.screen.width / 4) - (i >> 1);
+    cards[i].y = (app.screen.height / 2) - (i << 1);
+    app.stage.addChild(cards[i]);
+}
